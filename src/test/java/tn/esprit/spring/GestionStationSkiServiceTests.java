f9695299a3a266aca6c4e225662d428e14feebeb
hassen-prog <hassen.methnani@esprit.tn>
2023-11-08 22:04:31 +0100

    mockito test

---
+++ src/test/java/tn/esprit/spring/GestionStationSkiServiceTests.java
@@ -0,0 +1,48 @@
+package tn.esprit.spring;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Piste;
+import tn.esprit.spring.repositories.IPisteRepository;
+import tn.esprit.spring.services.PisteServicesImpl;
+
+
+import java.util.Optional;
+
+@SpringBootTest
+class GestionStationSkiServiceTests {
+
+    @Mock
+    private IPisteRepository pisteRepository;
+
+    @InjectMocks
+    private PisteServicesImpl pisteServices;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testRetrievePiste() {
+        // Create a product
+        Piste piste = new Piste();
+        piste.setNumPiste(1L);
+        piste.setNamePiste("Test Piste");
+
+        // Mock the behavior of the productRepository
+        Mockito.when(pisteRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(piste));
+
+        // Call the method to be tested
+        Piste retrievedProduct = pisteServices.retrievePiste(1L);
+
+        Assertions.assertNotNull(retrievedProduct);
+        Assertions.assertEquals("Test Piste", retrievedProduct.getNamePiste());
+    }
+}
